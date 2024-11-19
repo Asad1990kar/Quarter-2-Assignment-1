062c5920194ed996cd00e87110c72f1e3fb7f980
Asad1990kar <asad1990kar@gmail.com>
2024-11-19 21:14:49 +0500

    contact  font change

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,11 +2,11 @@ export default function Contact(){
     return(
         <div>
             <br/>
-            <h1 className="text-3xl text-center text-green-700 hover:font-semibold">
+            <h1 className="text-3xl font-serif text-center text-green-700 hover:font-semibold">
             E-mail : asad1990kar@gmail.com
             </h1>
             <br/>
-            <h2 className="text-3xl text-center text-green-700 hover:font-semibold">
+            <h2 className="text-3xl font-serif text-center text-green-700 hover:font-semibold">
             Mobile : 03043387406
             <br/>
             </h2>
